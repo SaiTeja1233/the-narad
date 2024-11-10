14b984fcc04c0599db079885d2b993a5bf546a3a
saiTeja1233 <k.saiteja1233@gmail.com>
2024-11-10 11:58:04 +0530

    new update

--- src/appwriteConfig.js
+++ src/appwriteConfig.js
@@ -4,7 +4,7 @@ export const client = new Client();
 
 client
     .setEndpoint("https://cloud.appwrite.io/v1")
-    .setProject("66de2f8c00245b8172dd"); // Replace with your project ID
+    .setProject("671bce910012a627c0e2"); // Replace with your project ID
 
 export const account = new Account(client);
 export { ID } from "appwrite";
