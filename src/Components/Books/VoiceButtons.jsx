b301a2392072d6a631632a4613c954063d7c2125
saiTeja1233 <k.saiteja1233@gmail.com>
2024-11-21 20:22:58 +0530

    voice update

--- src/Components/Books/VoiceButtons.jsx
+++ src/Components/Books/VoiceButtons.jsx
@@ -5,7 +5,7 @@ import "./Voicestyles.css";
 const voiceImages = {
     "Microsoft David": "/male1.png",
     "Microsoft Mark": "/male2.png",
-    "Microsoft Zira": "/femaleimg2.png",
+    "Microsoft Zira": "/female1.png",
     "Microsoft Heera": "/femaleimg2.png",
     "Google US English": "/female1.png",
 };
@@ -14,9 +14,8 @@ const voiceImages = {
 const voiceDisplayNames = {
     "Microsoft David": "Sitaram",
     "Microsoft Mark": "Arjun",
-    "Google US English": "Aadhya",
-    "Microsoft Zira": " Suryakantham",
-   
+    "Microsoft Zira": "Aadhya",
+    "Microsoft Heera": "Suryakantham",
 };
 
 const VoiceButtons = ({ onVoiceSelect }) => {
@@ -32,9 +31,8 @@ const VoiceButtons = ({ onVoiceSelect }) => {
                 isMale
                     ? voice.name.startsWith("Microsoft David") ||
                       voice.name.startsWith("Microsoft Mark")
-                    : voice.name.startsWith("Google US English") ||
+                    : voice.name.startsWith("Microsoft Heera") ||
                       voice.name.startsWith("Microsoft Zira")
-                    //   voice.name.startsWith("Microsoft Zira")
             );
             setVoices(filteredVoices);
         };
